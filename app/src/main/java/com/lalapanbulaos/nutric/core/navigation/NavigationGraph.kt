53ef40de7bd04beb647e3b34df5ba2c6f2aa71cb
kenziehh <ghaisani.nurani@gmail.com>
2024-11-21 03:30:18 +0700

    feat: add scaffold with route integration

--- app/src/main/java/com/lalapanbulaos/nutric/core/navigation/NavigationGraph.kt
+++ app/src/main/java/com/lalapanbulaos/nutric/core/navigation/NavigationGraph.kt
@@ -1,13 +1,65 @@
 package com.lalapanbulaos.nutric.core.navigation
 
+import android.provider.ContactsContract.Profile
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.lalapanbulaos.nutric.features.auth.presentation.screen.AuthScreen
+import com.lalapanbulaos.nutric.features.home.presentation.HomeScreen
+import com.lalapanbulaos.nutric.presentation.component.NutriCScaffold
+import com.lalapanbulaos.nutric.presentation.theme.NutriCTypography
+
+
+//@Composable
+//fun NavGraph(
+//    navController: NavController,
+//    startDestination: String = "home"
+//) {
+//    NavHost(navController = navController, startDestination = startDestination) {
+//        composable("home") {
+//            HomeScreen()
+//        }
+//        composable("mealstats") {
+////            MealStatsScreen()
+//        }
+//        composable("scanfood") {
+////            ScanFoodScreen()
+//        }
+//        composable("articles") {
+////            ArticleScreen {
+////                navController.navigate("articleDetail/$it")
+////            }
+//        }
+//        composable("profile") {
+////            ProfileScreen()
+//        }
+//    }
+//}
+@Composable
+fun ScanFoodScreen(){
+    Text("INI scan")
+}
 
 @Composable
-fun NavGraph(startDestination: String = "auth") {
+fun ArticleScreen(){
+    Text("INI artike")
+}
+@Composable
+fun StatiSticScreen() {
+    Text("INI statistic")
+}
+@Composable
+fun ProfileScreen() {
+    Text("INI profil")
+}
+
+
+
+
+@Composable
+fun NavGraph(startDestination: String = "home") {
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = startDestination) {
@@ -17,10 +69,39 @@ fun NavGraph(startDestination: String = "auth") {
 //                navController.navigate("signin")
 //            }
 //        }
-//
+
         composable("auth") {
             AuthScreen()
         }
+        composable("home") {
+            NutriCScaffold(navController = navController) {
+                HomeScreen()
+            }
+        }
+        composable("scanfood") {
+            NutriCScaffold(navController = navController) {
+                ScanFoodScreen()
+            }
+        }
+        composable("articles") {
+            NutriCScaffold(navController = navController) {
+                ArticleScreen()
+            }
+        }
+        composable("profile") {
+            NutriCScaffold(navController = navController) {
+                ProfileScreen()
+            }
+        }
+
+        composable("statistics") {
+            NutriCScaffold(navController = navController) {
+                StatiSticScreen()
+            }
+        }
+
+
+
 //
 //        composable("signup") {
 //            SignUpScreen {
@@ -35,13 +116,13 @@ fun NavGraph(startDestination: String = "auth") {
 //        }
 //
 //        composable("home") {
-//            HomeScreen(
-//                onScanFood = { navController.navigate("scanfood") },
-//                onArticle = { navController.navigate("articles") },
-//                onMealStats = { navController.navigate("mealstats") },
-//                onProfile = { navController.navigate("profile") },
-//                onCamera = { navController.navigate("camera") }
-//            )
+////            HomeScreen(
+////                onScanFood = { navController.navigate("scanfood") },
+////                onArticle = { navController.navigate("articles") },
+////                onMealStats = { navController.navigate("mealstats") },
+////                onProfile = { navController.navigate("profile") },
+////                onCamera = { navController.navigate("camera") }
+////            )
 //        }
 //
 //        composable("scanfood") {
---
+++ app/src/main/java/com/lalapanbulaos/nutric/presentation/component/Scaffold.kt
@@ -0,0 +1,132 @@
+package com.lalapanbulaos.nutric.presentation.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.lalapanbulaos.nutric.R
+import com.lalapanbulaos.nutric.presentation.theme.Colors
+
+data class BottomNavItem(
+    val name: String,
+    val iconActive: Int,
+    val iconInactive: Int
+)
+
+@Composable
+fun NutriCScaffold(navController: NavController, children: @Composable () -> Unit) {
+    Scaffold(
+        modifier = Modifier.safeDrawingPadding(),
+        bottomBar = { BottomNavbar(navController) }
+    ) { paddingValues ->
+        Box(modifier = Modifier.padding(paddingValues).padding(top = 40.dp, start = 24.dp, end = 24.dp).safeDrawingPadding()) {
+            children()
+        }
+    }
+}
+
+@Composable
+fun BottomNavbar(navController: NavController) {
+    // Get the current route from NavController
+    val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
+
+    val items = listOf(
+        BottomNavItem("Beranda", R.drawable.home_active, R.drawable.home_inactive),
+        BottomNavItem("Statistik", R.drawable.statistic_active, R.drawable.statistic_inactive),
+        BottomNavItem("Scan", R.drawable.scan, R.drawable.scan),
+        BottomNavItem("Artikel", R.drawable.artikel_active, R.drawable.artikel_inactive),
+        BottomNavItem("Profil", R.drawable.profile_active, R.drawable.profil_inactive)
+    )
+
+    // Determine the selected index based on the current route
+    val selectedItemIndex = when (currentRoute) {
+        "home" -> 0
+        "statistics" -> 1
+        "scanfood" -> 2
+        "articles" -> 3
+        "profile" -> 4
+        else -> 0 // Default to "home"
+    }
+
+    Surface(
+        modifier = Modifier.fillMaxWidth(),
+        shadowElevation = 8.dp,
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            items.forEachIndexed { index, item ->
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.weight(1f)
+                ) {
+                    if (index == 2) {
+                        Button(
+                            onClick = { navController.navigate("scanfood") },
+                            shape = CircleShape,
+                            colors = ButtonDefaults.buttonColors(containerColor = Colors.Secondary.color50),
+                            modifier = Modifier.size(72.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(id = if (selectedItemIndex == index) item.iconActive else item.iconInactive),
+                                contentDescription = item.name,
+                                modifier = Modifier.size(24.dp)
+                            )
+                        }
+                    } else {
+                        IconButton(
+                            onClick = {
+                                // Navigate to the corresponding screen
+                                when (index) {
+                                    0 -> navController.navigate("home")
+                                    1 -> navController.navigate("statistics")
+                                    3 -> navController.navigate("articles")
+                                    4 -> navController.navigate("profile")
+                                }
+                            }
+                        ) {
+                            Image(
+                                painter = painterResource(id = if (selectedItemIndex == index) item.iconActive else item.iconInactive),
+                                contentDescription = item.name,
+                                modifier = Modifier.size(24.dp)
+                            )
+                        }
+                    }
+                    Text(
+                        text = item.name,
+                        fontSize = 12.sp,
+                        color = if (selectedItemIndex == index)
+                            MaterialTheme.colorScheme.tertiary
+                        else
+                            MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                }
+            }
+        }
+    }
+}

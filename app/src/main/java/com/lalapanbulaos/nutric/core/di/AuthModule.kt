52af38087950c6dd2fe819494617fcb1a6d4d765
rmvs <nofalrmz@gmail.com>
2024-11-23 00:31:44 +0700

    feat: add Allergy module, service, repository, and UI components for allergy management

---
+++ app/src/main/java/com/lalapanbulaos/nutric/core/di/AuthModule.kt
@@ -0,0 +1,45 @@
+package com.lalapanbulaos.nutric.core.di
+
+import com.lalapanbulaos.nutric.core.data.local.pref.UserPreferencesManager
+import com.lalapanbulaos.nutric.features.auth.data.remote.AuthService
+import com.lalapanbulaos.nutric.features.auth.data.repository.AuthRepository
+import com.lalapanbulaos.nutric.features.auth.presentation.viewmodel.AuthViewModel
+import com.lalapanbulaos.nutric.features.auth.usecase.SignInUseCase
+import com.lalapanbulaos.nutric.features.auth.usecase.SignUpUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AuthModule {
+  @Provides
+  @Singleton
+  fun provideAuthService(retrofit: Retrofit): AuthService {
+    return retrofit.create(AuthService::class.java)
+  }
+
+  @Provides
+  @Singleton
+  fun provideAuthRepository(authService: AuthService): AuthRepository {
+    return AuthRepository(authService)
+  }
+
+  @Provides
+  fun provideSignInUseCase(authRepository: AuthRepository, userPreferencesManager: UserPreferencesManager): SignInUseCase {
+    return SignInUseCase(authRepository, userPreferencesManager)
+  }
+
+  @Provides
+  fun provideSignUpUseCase(authRepository: AuthRepository, userPreferencesManager: UserPreferencesManager): SignUpUseCase {
+    return SignUpUseCase(authRepository, userPreferencesManager)
+  }
+
+  @Provides
+  fun provideAuthViewModel(signInUseCase: SignInUseCase, signUpUseCase: SignUpUseCase): AuthViewModel {
+    return AuthViewModel(signInUseCase, signUpUseCase)
+  }
+}
---
+++ app/src/main/java/com/lalapanbulaos/nutric/features/healthinfo/presentation/component/AllergyItem.kt
@@ -0,0 +1,49 @@
+package com.lalapanbulaos.nutric.features.healthinfo.presentation.component
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.lalapanbulaos.nutric.features.healthinfo.data.model.Allergy
+import com.lalapanbulaos.nutric.presentation.theme.Colors
+
+@Composable
+fun AllergyItem(
+  allergy: Allergy,
+  onClick: () -> Unit, // Lambda to handle item click
+  isSelected: Boolean = false
+) {
+  Card(
+    modifier = Modifier
+      .fillMaxWidth()
+      .aspectRatio(1f) // Ensures square-shaped items
+      .clickable(onClick = onClick)
+      .border(
+        width = 1.dp,
+        color = if (isSelected) Colors.Primary.color40 else Colors.Neutral.color20,
+        shape = RoundedCornerShape(8.dp)
+      )
+    ,
+    shape = RoundedCornerShape(8.dp)
+  ) {
+    Box(
+      contentAlignment = Alignment.Center,
+      modifier = Modifier.padding(16.dp)
+    ) {
+      Text(
+        text = allergy.name, // Replace with the allergy's display name
+        textAlign = TextAlign.Center
+      )
+    }
+  }
+}

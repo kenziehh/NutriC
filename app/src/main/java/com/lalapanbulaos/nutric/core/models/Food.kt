4116565cbd1c0df8b5e68df576263df0c311e336
romves <nofalrmz@gmail.com>
2024-12-10 19:55:55 +0700

    feat: add data models for DailyTarget, Food, FoodMacroNutrient, and FoodMicroNutrient with serialization support

--- app/src/main/java/com/lalapanbulaos/nutric/core/models/Food.kt
+++ app/src/main/java/com/lalapanbulaos/nutric/core/models/Food.kt
@@ -1,13 +1,15 @@
 package com.lalapanbulaos.nutric.core.models
 
+import com.google.gson.annotations.SerializedName
+
 data class Food(
     val id: String,
     val name: String,
     val category: String,
+    @SerializedName("food_macronutrient")
     val foodMacroNutrient: FoodMacroNutrient?,
+    @SerializedName("food_micronutrient")
     val foodMicroNutrient: FoodMicroNutrient?,
     val allergens: List<Allergy>,
-    val mealLogs: List<MealLog>,
-    val scanHistories: List<ScanHistory>
 )
 

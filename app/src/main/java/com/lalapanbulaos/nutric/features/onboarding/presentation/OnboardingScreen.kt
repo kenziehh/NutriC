636d353bb0e4a8c75ef03241fbdf8e4c05d45973
romves <nofalrmz@gmail.com>
2024-12-12 04:26:22 +0700

    feat: enhance onboarding screen with dynamic steps and navigation functionality

--- app/src/main/java/com/lalapanbulaos/nutric/features/onboarding/presentation/OnboardingScreen.kt
+++ app/src/main/java/com/lalapanbulaos/nutric/features/onboarding/presentation/OnboardingScreen.kt
@@ -2,12 +2,14 @@ package com.lalapanbulaos.nutric.features.onboarding.presentation
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.content.MediaType.Companion.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -20,20 +22,31 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.lalapanbulaos.nutric.R
+import com.lalapanbulaos.nutric.features.auth.presentation.viewmodel.AuthViewModel
 import com.lalapanbulaos.nutric.presentation.component.NutriCButton
 import com.lalapanbulaos.nutric.presentation.theme.Colors
 import com.lalapanbulaos.nutric.presentation.theme.CustomTypography
+import com.lalapanbulaos.nutric.presentation.theme.NutriCTypography
 
 @Composable
 fun OnboardingScreen(
-    onGetStarted: () -> Unit
+    steps: List<OnboardingStep>,
+    currentStepIndex: Int,
+    onNext: () -> Unit,
+    onSkip: () -> Unit
 ) {
     Box(
         modifier = Modifier
@@ -46,25 +59,35 @@ fun OnboardingScreen(
                 .fillMaxSize()
                 .padding(bottom = 170.dp)
         )
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(id = steps[currentStepIndex].backgroundImage),
+                contentDescription = "background",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 10.dp)
+                    .align(Alignment.BottomCenter)
+                    .scale(1.2f)
+            )
+        }
     }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
+            .safeDrawingPadding()
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.onboarding_people1),
-            contentDescription = "background",
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(bottom = 120.dp)
-        )
-    }
-    Box(
-        modifier = Modifier
-            .fillMaxSize().safeDrawingPadding()
-    ) {
-        Text("Lewati",
-            modifier = Modifier.padding(start = 330.dp, top = 16.dp)
+        // Skip Button
+        Text(
+            text = "Lewati",
+            style = NutriCTypography.subHeadingSm,
+             modifier = Modifier
+                .padding(start = 330.dp, top = 16.dp)
+                .clickable { onSkip() }
         )
 
         Column(
@@ -79,51 +102,104 @@ fun OnboardingScreen(
                     top = 24.dp,
                     start = 24.dp,
                     end = 24.dp,
-                    bottom = 0.dp
+                    bottom = 24.dp
                 )
         ) {
             Spacer(modifier = Modifier.height(24.dp))
+
+            // Dots Indicator
             Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                repeat(3) { index ->
+                repeat(steps.size) { index ->
                     Box(
                         modifier = Modifier
                             .padding(horizontal = 4.dp)
                             .size(8.dp)
                             .background(
-                                color = if (index == 0) Colors.Primary.color40 else Colors.Neutral.color20,
+                                color = if (index == currentStepIndex) Colors.Primary.color40 else Colors.Neutral.color20,
                                 shape = CircleShape
                             )
                     )
                 }
             }
+
             Spacer(modifier = Modifier.height(32.dp))
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
+
+            // Step Content
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
                 Text(
-                    "Selamat Datang di NutriC!",
+                    text = steps[currentStepIndex].title,
                     style = CustomTypography.headlineLarge,
                     color = Color.Black
                 )
                 Spacer(modifier = Modifier.height(16.dp))
                 Text(
-                    "Teman setia untuk pemantauan dan perencanaan gizi Anda!",
+                    text = steps[currentStepIndex].description,
                     style = CustomTypography.bodyLarge,
                     color = Colors.Neutral.color30,
                     textAlign = TextAlign.Center
                 )
             }
+
             Spacer(modifier = Modifier.height(64.dp))
+
+            // Next Button
             NutriCButton(
-                onClick = onGetStarted,
+                onClick = onNext,
                 modifier = Modifier.fillMaxWidth(),
-                text = "Lanjut"
+                text = if (currentStepIndex == steps.size - 1) "Mulai" else "Lanjut"
             )
         }
     }
+}
+
+data class OnboardingStep(
+    val backgroundImage: Int,
+    val title: String,
+    val description: String
+)
+
+// Usage
+@Composable
+fun OnboardingStepsScreen(onFinish: () -> Unit, viewModel: AuthViewModel = hiltViewModel()) {
+    val steps = listOf(
+        OnboardingStep(
+            backgroundImage =
+            R.drawable.onboarding1,
+            title = "Selamat Datang di NutriC!",
+            description = "Teman setia untuk pemantauan dan perencanaan gizi Anda!"
+        ),
+        OnboardingStep(
+            backgroundImage = R.drawable.onboarding2,
+            title = "Pantau Asupan Anda",
+            description = "Dapatkan wawasan tentang pola makan Anda setiap hari."
+        ),
+        OnboardingStep(
+            backgroundImage = R.drawable.onboarding3,
+            title = "Rencanakan Gizi Anda",
+            description = "Atur tujuan dan rencana gizi yang sesuai untuk kebutuhan Anda."
+        )
+    )
+    var currentStep by remember { mutableStateOf(0) }
+
+    fun onOnboarded() {
+        viewModel.updateOnboardedState()
+        onFinish()
+    }
+
+    OnboardingScreen(
+        steps = steps,
+        currentStepIndex = currentStep,
+        onNext = {
+            if (currentStep < steps.size - 1) {
+                currentStep++
+            } else {
+                onOnboarded()
+            }
+        },
+        onSkip = { onOnboarded() }
+    )
 }
